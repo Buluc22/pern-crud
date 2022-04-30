16b32e8a8380e12a512c5faade89d43ff82b7246
Buluc22 <izquierdomarquez03@gmail.com>
2022-04-30 01:23:04 -0500

    Se realizo el formulario, el crear tareas, se tiene problemas con el css ya que no se genera los cambios, se dejo a lo ultimo, se seguira trabajando en las funciones del crud

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,17 +6,21 @@ export default function Navbar() {
     const navigate = useNavigate();
 
   return (
-    <div className="navbar">
+    <div className="header">
         <div className="nav-container">
-        <Link className="nav-logo" to={'/'}> 
+            
+            <label>
+            <Link className="" to={'/'}> 
                 PERN CRUD
             </Link>
-        </div>
-        <ul className="nav-menu">
+            </label>
+
+        <ul className="nav_links">
             <button className="button button1" onClick={ () => navigate ('/tasks/new')}>
                 Tasks
             </button>
         </ul>
+        </div>
     </div>
   )
 }
--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,7 +1,51 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function TaskForm() {
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+  });
+
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true)
+
+    const res = await fetch('http://localhost:4000/tasks', {
+      method: 'POST',
+      body: JSON.stringify(task),
+      headers: {"Content-Type": "application/json"},
+    })
+
+    const data = await res.json()
+    
+    setLoading(false)
+    navigate('/')
+
+  }; 
+
+  const handleChange = e => 
+    setTask({...task, [e.target.name]: e.target.value});
+  
+
   return (
     <div>
-        TaskForm
+        <form className="form" onSubmit={handleSubmit}>
+        <label>Titulo</label>
+        <input name="title" onChange={handleChange}></input>
+        
+        <label>Descripcion</label>
+        <input name="description" onChange={handleChange} />
+        <button type="submit" disabled={!task.title || !task.description}>
+          {loading ? 'Loading...' : 'Create'}
+        </button>
+        </form>
+
     </div>
   )
 }
